6605e4051080126cfceb2fee857ee005a7275328
what491683260 <317676250@qq.com>
2014-03-31 16:04:50 +0800

    en

--- 月光微盘/Controller/DVIWiFiTableViewController.m
+++ 月光微盘/Controller/DVIWiFiTableViewController.m
@@ -77,9 +77,13 @@
         while (temp_addr != NULL) {
             if( temp_addr->ifa_addr->sa_family == AF_INET) {
                 // Check if interface is en0 which is the wifi connection on the iPhone
-                if ([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"]) {
+                NSLog(@"%s", temp_addr->ifa_name);
+                if ([[NSString stringWithUTF8String:temp_addr->ifa_name] hasPrefix:@"en"]) {
                     // Get NSString from C String
                     address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                    if (address && [address length]) {
+                        break;
+                    }
                 }
             }
             
